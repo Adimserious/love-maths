7bbc3f44f01f7feebf7c3b009aab5082de2dee38
Adimserious <156024751+Adimserious@users.noreply.github.com>
2024-02-21 10:46:54 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswe() {
+
+}
+
+function calculateCorrectAnswe() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
